82d158d0c92904ccb127f9760307010a77050c51
André Ogle <ogleandre@gmail.com>
2022-08-04 15:50:15 +0200

    Add fastify with basic config and example .env file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello world');
+import Fastify from 'fastify';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const fastify = Fastify({ logger: true });
+
+fastify.get('/', async (_request, reply) => {
+  reply.type('application/json').code(200);
+  return { hello: 'world' };
+});
+
+const PORT = Number(process.env.PORT) || 3000;
+fastify.listen({ port: PORT }, (err, _address) => {
+  if (err) throw err;
+});
